ddbbaf633cf6f1fcd55336634b630ff4fa002064
GuJinLong <1107157131@qq.com>
2024-03-16 16:20:47 +0800

    66

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 import { Map } from 'maplibre-gl'
-
+interface drawFreehandType {
+  new (someProperty: Map): drawFreehand
+}
 export function setupCounter(element: HTMLButtonElement): void
 export declare function drawFreehand(map: Map): void
--- src/package/index.ts
+++ src/package/index.ts
@@ -1,8 +1,3 @@
-import { Map } from 'maplibre-gl'
 import { drawFreehand } from './utils/drawFree'
 
-interface drawFreehandType {
-  new (someProperty: Map): drawFreehand
-}
-
-export default drawFreehand as drawFreehandType
+export default drawFreehand
